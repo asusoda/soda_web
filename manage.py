081863c61269dd02ab1423631135d11795a62eab
Ben Juntilla <ben@benjuntilla.com>
2024-01-27 22:40:56 -0700

    scaffold wagtail

--- manage.py
+++ manage.py
@@ -1,22 +1,10 @@
-#!/gnu/store/rw6n86c008xqdbjs3nk4i7ggf6srdpgs-python-wrapper-3.10.7/bin/python
-"""Django's command-line utility for administrative tasks."""
+#!/usr/bin/env python
 import os
 import sys
 
+if __name__ == "__main__":
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "soda_web.settings.dev")
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soda_web.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    from django.core.management import execute_from_command_line
+
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-    main()
---
+++ soda_web/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-zjyuxzv-y$cut7gw2wh@np=-!6ap(x^btsra8i^#*p6uookan8"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
